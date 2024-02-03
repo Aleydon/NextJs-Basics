65f1af5d1572e4d2ae9b30f87d6d09c96b807128
Aleydon <aleydonroberto@gmail.com>
2024-02-03 13:41:51 -0300

    ADD: Using next/router

--- src/pages/routes/index.jsx
+++ src/pages/routes/index.jsx
@@ -1,6 +1,23 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function DefaultRoute() {
+	const router = useRouter();
+
+	function simpleNavigation(url) {
+		router.push(url);
+	}
+
+	function navigationWithParams() {
+		router.push({
+			pathname: '/routes/params',
+			query: {
+				id: '87342',
+				name: 'Maria'
+			}
+		});
+	}
+
 	return (
 		<>
 			<h2>Default Route</h2>
@@ -15,6 +32,33 @@ function DefaultRoute() {
 					<li>Navigate to Params Route as Aleydon</li>
 				</Link>
 			</ul>
+
+			<div
+				style={{
+					display: 'flex',
+					flexDirection: 'column',
+					alignItems: 'flex-start',
+					gap: 5
+				}}
+			>
+				<button
+					type="button"
+					onClick={() => router.push('/routes/14478/search')}
+				>
+					Search
+				</button>
+				<button
+					type="button"
+					onClick={() =>
+						simpleNavigation('/routes/params?id=87363&name=Daniel')
+					}
+				>
+					Daniel
+				</button>
+				<button type="button" onClick={navigationWithParams}>
+					Maria
+				</button>
+			</div>
 		</>
 	);
 }
